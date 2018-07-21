d7e23bb9c65c8522a98a7711e921662810627e0e
Ubuntu <ubuntu@vm.com>
2018-07-21 13:50:32 -0300

    Auth user and migrations added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
+            $table->unsignedTinyInteger('role')->default('0');
+
+            $table->unique(["email"], 'users_email_unique');
             $table->timestamps();
         });
     }
